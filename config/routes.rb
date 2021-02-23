1b262b4828f03d3fd84c614a259239bd3541d0ef
Nathan Menard <nathanmenard.work@gmail.com>
2021-02-23 11:55:05 +0100

    List all restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # See all restaurants
+  get "restaurants", to: "restaurants#index"
+
+  # See details about one restaurant
+  # Create a restaurant
+  # Update a restaurant
+  # Destroy a restaurant
 end
