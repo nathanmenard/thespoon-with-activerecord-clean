ab71d651667608c24880c37940d6322b94b6038b
Nathan Menard <nathanmenard.work@gmail.com>
2021-02-23 12:02:49 +0100

    Create a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,27 @@
 class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  # Strong params
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Create a restaurant
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+
   # See all restaurants
   get "restaurants", to: "restaurants#index"
 
   # See details about one restaurant
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
 
-  # Create a restaurant
   # Update a restaurant
   # Destroy a restaurant
 end
