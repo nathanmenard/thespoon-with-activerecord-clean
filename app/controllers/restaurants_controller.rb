c4a1de4428b19d0d0f528b7ae1691124bd186d04
Nathan Menard <nathanmenard.work@gmail.com>
2021-02-23 12:14:49 +0100

    Add filter to set restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def new
     @restaurant = Restaurant.new
   end
@@ -15,22 +17,18 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
 
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
 
     redirect_to restaurants_path
@@ -41,5 +39,9 @@ class RestaurantsController < ApplicationController
   # Strong params
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
